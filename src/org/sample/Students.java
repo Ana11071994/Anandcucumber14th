898ebdf5c26aedc3752b544b3d3f57194c192b82
Ana11071994 <anand11mech@gmail.com>
2022-10-14 14:10:32 +0530

    Students details

---
+++ src/org/sample/Students.java
@@ -0,0 +1,18 @@
+package org.sample;
+
+public class Students {
+	private void studentsId() {
+		System.out.println("student id is 827382");
+		
+
+	}
+	private void studentsName() {
+		System.out.println("student name is arun");
+
+	}
+	public static void main(String[] args) {
+		Students s = new Students();
+		
+	}
+
+}
